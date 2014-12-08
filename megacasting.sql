7fe514d9800dbada4a12d02b42c7367eea2e8eb1
MegaProjet <hardy.k@neuf.fr>
2014-12-08 16:47:51 +0100

    Version 1.1 de la BDD

--- megacasting.sql
+++ megacasting.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Client: localhost
--- Généré le: Jeu 04 Décembre 2014 à 08:44
--- Version du serveur: 5.6.12-log
--- Version de PHP: 5.4.16
+-- Client :  127.0.0.1
+-- Généré le :  Lun 08 Décembre 2014 à 16:36
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,10 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `megacasting`
+-- Base de données :  `megacasting`
 --
-CREATE DATABASE IF NOT EXISTS `megacasting` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `megacasting`;
 
 -- --------------------------------------------------------
 
@@ -36,7 +34,20 @@ CREATE TABLE IF NOT EXISTS `adresse` (
   `ville` varchar(100) NOT NULL,
   `location` varchar(100) NOT NULL,
   PRIMARY KEY (`id_adr`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Contenu de la table `adresse`
+--
+
+INSERT INTO `adresse` (`id_adr`, `numero`, `code_postal`, `rue`, `ville`, `location`) VALUES
+(1, 10, 53970, 'rue de la cannelle', 'lhuisserie', 'Pays de la loire'),
+(2, 10, 72000, 'rue des tatata', 'paris', 'france'),
+(3, 30, 54123, 'rue des titititi', 'marseille', 'france'),
+(4, 60, 35415, 'rue des trotrotro', 'paris', 'france'),
+(5, 20, 78541, 'rue des importer', 'lyon', 'france'),
+(6, 45, 98765, 'rue des exporter', 'lille', 'france'),
+(7, 65, 32165, 'rue des privilèges', 'Brest', 'france');
 
 -- --------------------------------------------------------
 
@@ -54,7 +65,16 @@ CREATE TABLE IF NOT EXISTS `annonceur` (
   PRIMARY KEY (`id_annonceur`),
   KEY `id_media` (`id_media`),
   KEY `id_adresse` (`id_adresse`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `annonceur`
+--
+
+INSERT INTO `annonceur` (`id_annonceur`, `nom`, `mail`, `telephone`, `id_adresse`, `id_media`) VALUES
+(2, 'roger', 0, '0642250412', 1, 2),
+(3, 'mael', 0, '070541212', 2, 3),
+(4, 'remi', 0, '845145612', 3, 4);
 
 -- --------------------------------------------------------
 
@@ -79,7 +99,15 @@ CREATE TABLE IF NOT EXISTS `contrat` (
   `id_contrat` bigint(20) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(200) NOT NULL,
   PRIMARY KEY (`id_contrat`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `contrat`
+--
+
+INSERT INTO `contrat` (`id_contrat`, `libelle`) VALUES
+(1, 'CDI'),
+(2, 'CDD');
 
 -- --------------------------------------------------------
 
@@ -97,7 +125,15 @@ CREATE TABLE IF NOT EXISTS `diffuseur` (
   PRIMARY KEY (`id_diffuseur`),
   KEY `id_media` (`id_media`),
   KEY `id_adresse` (`id_adresse`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `diffuseur`
+--
+
+INSERT INTO `diffuseur` (`id_diffuseur`, `nom`, `mail`, `telephone`, `id_adresse`, `id_media`) VALUES
+(1, 'Igor', 0, '0642259845', 4, 2),
+(2, 'Tomi', 0, '0214512541', 5, 3);
 
 -- --------------------------------------------------------
 
@@ -109,7 +145,16 @@ CREATE TABLE IF NOT EXISTS `domaine` (
   `id_domaine` bigint(20) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(200) NOT NULL,
   PRIMARY KEY (`id_domaine`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `domaine`
+--
+
+INSERT INTO `domaine` (`id_domaine`, `libelle`) VALUES
+(1, 'Musique'),
+(2, 'Danse'),
+(3, 'Cinema');
 
 -- --------------------------------------------------------
 
@@ -202,7 +247,16 @@ CREATE TABLE IF NOT EXISTS `media` (
   `id_type` bigint(20) NOT NULL,
   PRIMARY KEY (`id_media`),
   KEY `id_offre` (`id_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `media`
+--
+
+INSERT INTO `media` (`id_media`, `nom`, `taille`, `id_type`) VALUES
+(2, 'media 1', 100, 1),
+(3, 'media 2', 524, 2),
+(4, 'media 3', 3654, 3);
 
 -- --------------------------------------------------------
 
@@ -272,7 +326,16 @@ CREATE TABLE IF NOT EXISTS `metier` (
   `id_domaine` bigint(20) NOT NULL,
   PRIMARY KEY (`id_metier`),
   KEY `id_domaine` (`id_domaine`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `metier`
+--
+
+INSERT INTO `metier` (`id_metier`, `libelle`, `id_domaine`) VALUES
+(1, 'musicien', 1),
+(2, 'danseur', 2),
+(3, 'réalisateur', 3);
 
 -- --------------------------------------------------------
 
@@ -285,6 +348,7 @@ CREATE TABLE IF NOT EXISTS `musique` (
   `titre` varchar(100) NOT NULL,
   `description` varchar(500) NOT NULL,
   `genre` varchar(100) NOT NULL,
+  `date_ajout` date NOT NULL,
   `id_utilisateur` bigint(20) NOT NULL,
   PRIMARY KEY (`id_musique`),
   KEY `media` (`id_utilisateur`)
@@ -302,7 +366,7 @@ CREATE TABLE IF NOT EXISTS `offre` (
   `reference` int(11) NOT NULL,
   `date_debut_publi` datetime NOT NULL,
   `fin_publi` datetime NOT NULL,
-  `nbr_post` int(11) NOT NULL,
+  `nbr_poste` int(11) NOT NULL,
   `descr_poste` varchar(500) NOT NULL,
   `descr_profil` varchar(500) DEFAULT NULL,
   `duree_contrat` int(11) DEFAULT NULL,
@@ -315,7 +379,15 @@ CREATE TABLE IF NOT EXISTS `offre` (
   KEY `id_annonceur` (`id_annonceur`),
   KEY `id_diffuseur` (`id_diffuseur`),
   KEY `id_metier` (`id_metier`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `offre`
+--
+
+INSERT INTO `offre` (`id_offre`, `titre`, `reference`, `date_debut_publi`, `fin_publi`, `nbr_poste`, `descr_poste`, `descr_profil`, `duree_contrat`, `id_contrat`, `id_annonceur`, `id_diffuseur`, `id_metier`) VALUES
+(1, 'Symphonia du blablabla', 355, '2014-11-01 00:00:00', '2014-11-30 00:00:00', 3, 'pianiste', 'Recherche un pianiste avec +10 expériences', 365, 2, 2, 1, 1),
+(2, 'Just Dance', 654, '2015-01-01 00:00:00', '2015-03-18 00:00:00', 10, 'danseur hip-hop', 'Recherche un pianiste avec au moins 1 an de pratique et qui connais le jeux just dance', 50, 2, 3, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -367,7 +439,16 @@ CREATE TABLE IF NOT EXISTS `type_media` (
   `id_type` bigint(20) NOT NULL AUTO_INCREMENT,
   `format` varchar(10) NOT NULL,
   PRIMARY KEY (`id_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `type_media`
+--
+
+INSERT INTO `type_media` (`id_type`, `format`) VALUES
+(1, 'jpeg'),
+(2, 'mp3'),
+(3, 'mkv');
 
 -- --------------------------------------------------------
 
@@ -389,7 +470,14 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `descr_util` varchar(500) NOT NULL,
   PRIMARY KEY (`id_util`),
   KEY `adresse` (`id_adresse`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id_util`, `login`, `password`, `nom`, `prenom`, `nom_artiste`, `age`, `mail`, `telephone`, `id_adresse`, `descr_util`) VALUES
+(2, 'admin', 'admin', 'pauchard', 'thomas', 'tpauch', 19, 'admin', '0642250575', 1, 'pas de description');
 
 -- --------------------------------------------------------
 
